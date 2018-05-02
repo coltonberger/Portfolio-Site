e53981d0118a1ec1d7afbbb739038fc424cae755
Colton Berger <coltonberger@optonline.net>
2018-05-02 10:55:57 -0400

    added more

---
+++ index.js
@@ -0,0 +1,13 @@
+// window.onload = function() {
+//   let allListItems = document.querySelectorAll("li");
+//   allListItems.forEach((listItem) =>{
+//     listItem.style.backgroundColor = "yellow"
+//     listItem.addEventListener("click", namedFunction);
+//   })
+//   function namedFunction(event){
+//   let elem = document.querySelector(".selected")
+//     if(elem) {
+//       elem.className = "";
+//     }
+//       this.className = "selected"
+// }
